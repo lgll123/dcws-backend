b9065c97c5999717aeab92e1674aaa543369b1ad
yqh <yangqingheng@fox.com>
2025-03-05 08:49:41 +0800

    部门分管领导代码修改

---
+++ dcws-modules/dcws-workflow/src/main/java/com/formssi/workflow/externalsystem/assets/listener/AssetsApplyTaskExeListener.java
@@ -0,0 +1,40 @@
+package com.formssi.workflow.externalsystem.assets.listener;
+
+import cn.hutool.core.convert.Convert;
+import com.formssi.common.core.service.DeptService;
+import com.formssi.common.satoken.utils.LoginHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.ExecutionListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 采购任务启动
+ */
+@Slf4j
+@Component("AssetsApplyTaskExeListener")
+public class AssetsApplyTaskExeListener implements ExecutionListener {
+    @Autowired
+    private DeptService deptService;
+    @Override
+    public void notify(DelegateExecution delegateTask) {
+        try{
+            Long deptId = LoginHelper.getDeptId();
+            Long respLeader = deptService.selectDeptRespLeaderById(Convert.toStr(deptId));
+            Long leader = deptService.selectDeptLeaderById(Convert.toStr(deptId));
+            delegateTask.setVariable("leader", leader);
+            delegateTask.setVariable("respLeader", respLeader);
+            delegateTask.setVariable("userId", LoginHelper.getUserId());
+            if(respLeader==null || leader==null){
+                log.error("An error occurred while AssetsApplyTaskExeListener respLeader: "+respLeader+" leader:"+leader);
+            }
+        } catch (Exception e) {
+            log.error("An error occurred while AssetsApplyTaskExeListener", e);
+        }
+    }
+
+
+
+
+}

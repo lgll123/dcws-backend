4aa3a11155b10c684345b5eef7240fe3e3cba526
yqh <yangqingheng@fox.com>
2025-03-20 16:17:22 +0800

    服务申请流程启动流程

--- dcws-modules/dcws-workflow/src/main/java/com/formssi/workflow/externalsystem/assets/servicewrapper/IActTaskServiceWrapper.java
+++ dcws-modules/dcws-workflow/src/main/java/com/formssi/workflow/externalsystem/assets/servicewrapper/IActTaskServiceWrapper.java
@@ -1,10 +1,17 @@
 package com.formssi.workflow.externalsystem.assets.servicewrapper;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DateUtil;
+import com.formssi.common.core.enums.BusinessStatusEnum;
+import com.formssi.workflow.domain.DcwsProjectTask;
 import com.formssi.workflow.domain.bo.CompleteTaskBo;
-import com.formssi.workflow.domain.bo.DcwsCompleteTaskBo;
+import com.formssi.workflow.domain.bo.DcwsProjectBo;
 import com.formssi.workflow.domain.bo.StartProcessBo;
-import com.formssi.workflow.service.DcwsIActTaskService;
+import com.formssi.workflow.domain.vo.DcwsProjectVo;
+import com.formssi.workflow.mapper.DcwsProjectTaskMapper;
 import com.formssi.workflow.service.IActTaskService;
+import com.formssi.workflow.service.ProjectManagementService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -13,18 +20,12 @@ import java.util.Map;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class IActTaskServiceWrapper {
     private final IActTaskService actTaskService;  // 原有服务（含@Transactional的方法）
+    private final ProjectManagementService projectManagementService;  // 原有服务（含@Transactional的方法）
     private final TransactionTemplate transactionTemplate;  // 编程式事务模板
-
-    public IActTaskServiceWrapper(
-            IActTaskService actTaskService,
-            DcwsIActTaskService dcwsIActTaskService,
-            TransactionTemplate transactionTemplate
-    ) {
-        this.actTaskService = actTaskService;
-        this.transactionTemplate = transactionTemplate;
-    }
+    private final DcwsProjectTaskMapper dcwsProjectTaskMapper;
 
     /**
      * 在新事务中调用原方法，异常仅回滚新事务
@@ -60,5 +61,33 @@ public class IActTaskServiceWrapper {
                 return false;  // 返回值
             }
         }));
+    }
+
+    /**
+     * 在新事务中调用原方法，异常仅回滚新事务
+     * 启动项目
+     */
+    public Integer startProjectInNewTransaction(DcwsProjectBo projectBo, Map<String,Object> taskNodeData) {
+        return transactionTemplate.execute(status -> {
+            try {
+                DcwsProjectVo dcwsProjectVo = projectManagementService.insertByBo(projectBo);// 调用原方法（已注解@Transactional）
+                DcwsProjectTask dcwsProjectTask = new DcwsProjectTask();
+                dcwsProjectTask.setTaskType(Convert.toStr(taskNodeData.get("applyType")));
+                dcwsProjectTask.setProjectId(dcwsProjectVo.getProjectId());
+                dcwsProjectTask.setTaskName("服务申请");
+                dcwsProjectTask.setTaskStatus(BusinessStatusEnum.WAITING.getStatus());
+                dcwsProjectTask.setCreateBy(Convert.toLong(taskNodeData.get("applicantId")));
+                dcwsProjectTask.setCreateTime(DateUtil.parse(Convert.toStr(taskNodeData.get("applyDate"))));
+                dcwsProjectTask.setCreateEmpName(Convert.toStr(taskNodeData.get("applicant")));
+                dcwsProjectTask.setBusinessKey(Convert.toStr(taskNodeData.get("id")));
+               return dcwsProjectTaskMapper.insert(dcwsProjectTask);
+            } catch (Exception e) {
+                status.setRollbackOnly();  // 标记事务回滚
+                // 记录日志，但无需处理（新事务已标记回滚）
+                // 记录失败记录，待auto处理启动流程 TODO
+                log.info("项目启动失败: " + e.getMessage());
+            }
+            return 1;
+        });
     }
 }
